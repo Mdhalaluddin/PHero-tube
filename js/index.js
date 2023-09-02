8bb621ca20b6af9799b7f54ffc47f2dbbc523e4e
Halal1122 <mdhealaluddin505@gmail.com>
2023-09-02 17:59:32 +0600

    card news add

--- js/index.js
+++ js/index.js
@@ -10,15 +10,9 @@ const categoryApi = async () => {
     <a onclick="handleNews(('${category.category_id}'))" id"btn-click" class="tab btn normal-case text-lg font-medium bg-slate-200 m-4 ">${category.category}</a> 
     
     `
-    // <button onclick="drawingBtn(id)">${category.category}</button>
         tabContainer.appendChild(div)
     })
-    // if(tabContainer && data.data){
-    //     console.log(data.data)
-    // }
-    // else{
-    //     console.log('no data')
-    // }
+    
 }
 
 
@@ -32,19 +26,18 @@ function htmlBlog(){
 const handleNews = async (categoryId) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
     const data = await res.json();
-    console.log(data)
-    // const noData = document.getElementById('no-data');
-    // noData.innerHTML = " ";
+    // console.log(data.data.length)
+    // ---------------------no data----------------
+    const noDataPage = document.getElementById('no-data')
+    noDataPage.innerHTML="";
     if(data.data.length === 0){
-        const div = createElement.innerHTML('div')
-        div.innerHTML =  `
-        <img src="img/onData.svg" alt="">
-        <h1 class="text-4xl font-bold">Oops!! Sorry, There is no content here</h1>
-        `
-        // noData.appendChild(div);
-    }else{
-        
-    }
+        const nav = document.createElement('nav');
+                nav.innerHTML =  `
+                <img class="mt-8 lg:mx-60 mx-36" src="img/onData.svg" alt="">
+                <h1 class="text-4xl font-bold mt-8 ">Oops!! Sorry, There is no content here</h1>
+                `
+    noDataPage.appendChild(nav);
+            }
     const cardHandle = document.getElementById('card-container')
     cardHandle.innerHTML = " ";
     data.data.forEach((news) => {
@@ -52,25 +45,24 @@ const handleNews = async (categoryId) => {
         const div = document.createElement('div');
         div.innerHTML = `
             <div class="card w-full item-center mt-8 h-4/5 bg-base-100 shadow-xl">
-                        <figure class="flex h-3/4">
+                    <figure class="flex h-3/4">
                         <div class="hero">
-                        <img class="w-full h-[280px]  " src="${news?.thumbnail}" />
+                         <img class="w-full h-[280px]  " src="${news?.thumbnail}" />
                         <div class="absolute left-[250px]  inset-x-9 bottom-52 rounded-md">
                             <div class="max-w-md bg-stone-200">
                                 <p onclick="showTime(sec)" class=" ">${news.others.posted_date?"${array[0]}hours and ${array[1]" : ""}
-                                
                                 </p>
                             </div>
                         </div>
                         </div>
-                        </figure>
+                    </figure>
                         <div class="card-body">
                             <h2 class="card-title">
                             ${news?.title}
                             </h2>
                             <div class="flex items-center">
                                 <div class="rounded-full w-14">
-                                    <img class="w-14 rounded-full " src="${news?.authors[0].profile_picture}">
+                                    <img class="w-14 h-14 rounded-full " src="${news?.authors[0].profile_picture}">
                                 </div>
                                 <h2 class="text-sm font-normal px-4">${news.authors[0].profile_name}</h2>
                                 <div class="">
@@ -78,7 +70,7 @@ const handleNews = async (categoryId) => {
                                 </div>
                             </div>
                             <div class="card-actions justify-end">
-                            <p>${news.others.views} views</p>
+                            <p onclick="sortBtn(data)">${news.others.views} views</p>
                             
                             
                             </div>
@@ -89,13 +81,25 @@ const handleNews = async (categoryId) => {
         cardHandle.appendChild(div);
     })
 }
-function showTime(sec){
-    let hrs = sec / 36000;
-    let min = sec % 3600 / 60;
-    let secs = sec % 3600 % 60;
-    return `<div>${hrs}hrs ${min}min ${secs}</div>`;
-    }
-   console.log(showTime, 60000);
+
+let x = 0;
+const sortBtn = (data) =>{
+
+}
+sortBtn()
+
+
+
+
+
+
+// function showTime(sec){
+//     let hrs = sec / 36000;
+//     let min = sec % 3600 / 60;
+//     let secs = sec % 3600 % 60;
+//     return `<div>${hrs}hrs ${min}min ${secs}</div>`;
+//     }
+//    console.log(showTime, 60000);
 
 handleNews("1000")
 
